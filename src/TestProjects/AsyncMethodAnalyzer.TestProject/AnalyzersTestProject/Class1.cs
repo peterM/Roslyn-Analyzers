0ea8b1c3859b0d7c08cc5dce1561b584d67c7f7f
Peter Malík <p.malik@malikp.eu>
2019-04-13 09:37:52 +0200

    Add Test project

---
+++ src/TestProjects/AsyncMethodAnalyzer.TestProject/AnalyzersTestProject/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AnalyzersTestProject
+{
+    public interface IInterface
+    {
+        Task MyMethodAsync(string s, System.Threading.CancellationToken cancellationToken);
+    }
+
+    public class Class1 : IInterface
+    {
+        public Class1()
+        {
+        }
+
+        public Task MyMethodAsync(string a, System.Threading.CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
