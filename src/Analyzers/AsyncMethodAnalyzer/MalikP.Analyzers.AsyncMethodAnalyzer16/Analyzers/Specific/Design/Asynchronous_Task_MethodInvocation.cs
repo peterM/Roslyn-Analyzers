995fb3f61e299e35bb61c7ece056e32169f91df4
Peter Malík <p.malik@malikp.eu>
2019-04-17 16:10:32 +0200

    Add new analyzers - preparation

---
+++ src/Analyzers/AsyncMethodAnalyzer/MalikP.Analyzers.AsyncMethodAnalyzer16/Analyzers/Specific/Design/Asynchronous_Task_MethodInvocation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace MalikP.Analyzers.AsyncMethodAnalyzer.Analyzers.Specific.Design
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public sealed class Asynchronous_Task_MethodInvocation : AbstracSyntaxNodeActionDiagnosticAnalyzer
+    {
+        protected override SyntaxKind[] SyntaxKinds =>
+            new[]
+            {
+                SyntaxKind.InvocationExpression
+            };
+
+        protected override DiagnosticDescriptor DiagnosticDescriptor => null;
+
+        protected override void AnalyzeNode(SyntaxNodeAnalysisContext context)
+        {
+        }
+    }
+}
---
+++ src/Analyzers/AsyncMethodAnalyzer/MalikP.Analyzers.AsyncMethodAnalyzer16/Analyzers/Specific/Design/Asynchronous_Void_MethodDeclaration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace MalikP.Analyzers.AsyncMethodAnalyzer.Analyzers.Specific.Design
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public sealed class Asynchronous_Void_MethodDeclaration : AbstracSyntaxNodeActionDiagnosticAnalyzer
+    {
+        protected override SyntaxKind[] SyntaxKinds =>
+            new[]
+            {
+                SyntaxKind.MethodDeclaration
+            };
+
+        protected override DiagnosticDescriptor DiagnosticDescriptor => null;
+
+        protected override void AnalyzeNode(SyntaxNodeAnalysisContext context)
+        {
+        }
+    }
+}
