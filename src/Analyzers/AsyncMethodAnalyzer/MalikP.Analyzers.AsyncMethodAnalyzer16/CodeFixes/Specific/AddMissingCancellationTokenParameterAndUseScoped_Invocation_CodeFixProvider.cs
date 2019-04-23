196ada9906e537aff3522937f94814d3db8243ae
Peter M <p.malik@malikp.eu>
2019-04-23 23:37:33 +0200

    3 - Codefix provider do not update invocation (#3)

--- src/Analyzers/AsyncMethodAnalyzer/MalikP.Analyzers.AsyncMethodAnalyzer16/CodeFixes/Specific/AddMissingCancellationTokenParameterAndUseScoped_Invocation_CodeFixProvider.cs
+++ src/Analyzers/AsyncMethodAnalyzer/MalikP.Analyzers.AsyncMethodAnalyzer16/CodeFixes/Specific/AddMissingCancellationTokenParameterAndUseScoped_Invocation_CodeFixProvider.cs
@@ -31,21 +31,19 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
+using MalikP.Analyzers.AsyncMethodAnalyzer.Models;
 using MalikP.Analyzers.AsyncMethodAnalyzer.Rules.Design;
 
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.Options;
-using Microsoft.CodeAnalysis.Simplification;
 
 namespace MalikP.Analyzers.AsyncMethodAnalyzer.CodeFixes.Specific
 {
     [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(AddMissingCancellationTokenParameterAndUseScoped_Invocation_CodeFixProvider)), Shared]
-    public sealed class AddMissingCancellationTokenParameterAndUseScoped_Invocation_CodeFixProvider : AbstractSolutionCodefixProvider<InvocationExpressionSyntax>
+    public sealed class AddMissingCancellationTokenParameterAndUseScoped_Invocation_CodeFixProvider : AbstractAddMissingCancellationTokenParameter_Invocation_CodeFixProvider
     {
-        private const string _identifierName = "cancellationToken";
         private const string _identifierType = "CancellationToken";
 
         protected override string Title => "Add 'CancellationToken' from scope";
@@ -57,73 +55,33 @@ namespace MalikP.Analyzers.AsyncMethodAnalyzer.CodeFixes.Specific
                 CancellationTokenParameterReusePossibility_Void_Invocation_Rule.DiagnosticId
             };
 
-        protected override async Task<Solution> ChangedSolutionHandlerAsync(Document document, InvocationExpressionSyntax syntaxDeclaration, CancellationToken cancellationToken)
+        protected override async Task<SyntaxNodeReplacementPair> ConstructInvocationPairAsync(Document document, InvocationExpressionSyntax syntaxDeclaration, CancellationToken cancellationToken)
         {
             SyntaxNode root = await GetRootAsync(document, cancellationToken).ConfigureAwait(false);
-
             BaseMethodDeclarationSyntax parentMethodSyntax = GetSyntaxes<BaseMethodDeclarationSyntax>(root, syntaxDeclaration.Span)
                 .FirstOrDefault();
 
             if (parentMethodSyntax == null)
             {
-                return document.Project.Solution;
+                return null;
             }
 
             List<ParameterSyntax> parameters = parentMethodSyntax.ParameterList.Parameters.ToList();
             ParameterSyntax cancellationTokenParameter = parameters.FirstOrDefault(CompareParameter);
-
-            ArgumentSyntax newArgument = SyntaxFactory.Argument(SyntaxFactory.ParseExpression(cancellationTokenParameter.Identifier.Text));
+            ArgumentSyntax newArgument = CreateArgument(cancellationTokenParameter);
             InvocationExpressionSyntax updatedMethodInvocation = syntaxDeclaration.AddArgumentListArguments(newArgument);
 
-            SyntaxTree syntaxTree = await document.GetSyntaxTreeAsync(cancellationToken).ConfigureAwait(false);
-
-            SyntaxNode updatedSyntaxTree = syntaxTree
-                .GetRoot()
-                .ReplaceNode(syntaxDeclaration, updatedMethodInvocation);
-
-            Solution solution = document.Project.Solution.WithDocumentSyntaxRoot(document.Id, updatedSyntaxTree);
-
-            return await AddCancellationTokenToDeclaringMethod(solution, document, syntaxDeclaration, cancellationToken).ConfigureAwait(false);
+            return new SyntaxNodeReplacementPair(document, root, syntaxDeclaration, updatedMethodInvocation);
         }
 
-        private async Task<Solution> AddCancellationTokenToDeclaringMethod(Solution solution, Document document, InvocationExpressionSyntax syntaxDeclaration, CancellationToken cancellationToken)
-        {
-            SemanticModel semanticModel = await document.GetSemanticModelAsync(cancellationToken).ConfigureAwait(false);
-            ISymbol symbol = semanticModel.GetSymbolInfo(syntaxDeclaration).Symbol;
-
-            SyntaxReference declaringSyntax = symbol.DeclaringSyntaxReferences.FirstOrDefault();
-
-            MethodDeclarationSyntax methodSyntax = declaringSyntax.GetSyntax(cancellationToken) as MethodDeclarationSyntax;
-
-            SyntaxToken identifier = SyntaxFactory.Identifier(_identifierName);
-            TypeSyntax typeName = SyntaxFactory.ParseTypeName(typeof(CancellationToken).FullName);
-
-            ParameterSyntax parameter = SyntaxFactory
-                .Parameter(identifier)
-                .WithType(typeName)
-                .WithAdditionalAnnotations(Simplifier.Annotation);
-
-            MethodDeclarationSyntax updatedMethod = methodSyntax.AddParameterListParameters(parameter);
-
-            SyntaxNode updatedSyntaxTree = declaringSyntax.SyntaxTree
-                .GetRoot()
-                .ReplaceNode(methodSyntax, updatedMethod);
-
-            document = document.Project.Solution.GetDocument(declaringSyntax.SyntaxTree);
-            document = document.WithSyntaxRoot(updatedSyntaxTree);
-
-            DocumentOptionSet options = await document.GetOptionsAsync(cancellationToken).ConfigureAwait(false);
-
-            document = await Simplifier.ReduceAsync(document, options, cancellationToken).ConfigureAwait(false);
-
-            updatedSyntaxTree = await document.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
-
-            return solution.WithDocumentSyntaxRoot(document.Id, updatedSyntaxTree);
-        }
-
-        private static bool CompareParameter(ParameterSyntax parameterSyntax)
+        private bool CompareParameter(ParameterSyntax parameterSyntax)
         {
             return parameterSyntax.Type.ToString().Contains(_identifierType);
+        }
+
+        private ArgumentSyntax CreateArgument(ParameterSyntax cancellationTokenParameter)
+        {
+            return SyntaxFactory.Argument(SyntaxFactory.ParseExpression(cancellationTokenParameter.Identifier.Text));
         }
     }
 }
